7b3d11d7bab23b1525bf89ac463c1a2605aadf39
Irina Garmaeva <garmaeva-i@mail.ru>
2023-11-09 15:31:25 +0100

    Feature: add chat activity by brodcasting events
    show users who is typing a message

--- server/index.js
+++ server/index.js
@@ -22,12 +22,30 @@ const io = new Server(expressServer, {
   }
 })
 
+
 io.on('connection', socket => {
   console.log(`User ${socket.id} connected`)
 
+  //Upon connection - only to connected user
+  socket.emit('message', "Welcome to Chat App!")
+
+  //Upon connection - only to all others
+  socket.broadcast.emit('message', `User ${socket.id} connected`)
+  
+  //Listening for a message event
   socket.on('message', data => {
     console.log(data)
     io.emit('message', `${socket.id.substring(0,5)}: ${data}`)
+  })
+
+  //Listen to activity
+  socket.on('activity', (name) => {
+    socket.broadcast.emit('activity', name)
+  })
+
+  //When user disconnects - to all others
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('message', `User ${socket.id} disconnected`)
   })
 })
 
--- server/public/app.js
+++ server/public/app.js
@@ -1,19 +1,38 @@
 const socket = io('ws://localhost:3500')
 
+const activity = document.querySelector('.activity')
+const messageInput = document.querySelector('input')
+
 function sendMesssage (e) {
   e.preventDefault()
-  const input = document.querySelector('input')
-  if(input.value) {
-    socket.emit('message', input.value)
-    input.value = ''
+
+  if(messageInput.value) {
+    socket.emit('message', messageInput.value)
+    messageInput.value = ''
   }
-  input.focus()
+  messageInput.focus()
 }
 
 document.querySelector('form').addEventListener('submit', sendMesssage)
 
 socket.on('message', (data) => {
+  activity.textContent = ''
   const li = document.createElement('li')
   li.textContent = data
   document.querySelector('ul').appendChild(li)
+})
+
+messageInput.addEventListener('keypress', () => {
+  socket.emit('activity', socket.id.substring(0, 5))
+})
+
+let activityTimer
+socket.on('activity', (name) => {
+  activity.textContent = `${name} is typing...`
+  
+  // Clear after 3 seconds
+  clearTimeout(activityTimer)
+  activityTimer = setTimeout(() => {
+    activity.textContent = ""
+  }, 3000)
 })
